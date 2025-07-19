5f5ec4969da35f4cc3cb2e02d75fa94e27170987
allwin23 <allwinjoe702@gmail.com>
2025-07-19 02:49:41 -0400

    innitla commit

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react"
+
+const url ='https://api.npoint.io/9045c260b1565daa9e15'
 const UseFetch = (url) => {
-    
+    const[data,setData] = useState();
+    useEffect(()=>{
+        fetch(url).then((res)=>res.json())
+        .then((data)=>setData(data))
+    },[])
+return [data]
 }
 
 export default UseFetch
