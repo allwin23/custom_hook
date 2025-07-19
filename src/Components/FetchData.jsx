1ccedc908047b48a329c79298866987e0bd771be
allwin23 <allwinjoe702@gmail.com>
2025-07-19 02:55:18 -0400

    fetch successful

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import useFetch from './UseFetch';
 const FetchData = () => {
+     const [data]=useFetch('https://api.npoint.io/9045c260b1565daa9e15');
+ console.log(data);
   return (
     <>
      <ul className='list_data_main'>
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
      </ul>
+     
     </>
   )
 }
--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react'
 
-const url ='https://api.npoint.io/9045c260b1565daa9e15'
-const UseFetch = (url) => {
-    const[data,setData] = useState();
+ const useFetch = (url) => {
+    const[data,setData]=useState();
     useEffect(()=>{
         fetch(url).then((res)=>res.json())
         .then((data)=>setData(data))
     },[])
-return [data]
+  return [data]
 }
 
-export default UseFetch
+export default useFetch
